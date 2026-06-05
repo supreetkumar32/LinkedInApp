64d0071ccee83d6f0b60f986543206a3ce170e78
Supreet <supreetkumar32@gmail.com>
2026-06-05 11:02:47 +0530

    skeleton for user service

---
+++ user-service/src/main/java/com/project/linkedIn/user_service/exception/BadRequestException.java
@@ -0,0 +1,7 @@
+package com.project.linkedIn.user_service.exception;
+
+public class BadRequestException extends RuntimeException{
+    public BadRequestException(String message) {
+        super(message);
+    }
+}
---
+++ user-service/src/main/java/com/project/linkedIn/user_service/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.project.linkedIn.user_service.exception;
+
+public class ResourceNotFoundException extends RuntimeException{
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
